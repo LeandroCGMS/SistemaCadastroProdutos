13d2c804973a58156fca2963b83437aaa68150e1
LeandroCGMS <leandrocgms1986@outlook.com>
2018-04-15 11:18:30 -0400

    Iniciando em GUI com Java.

---
+++ src/sistemacadastroprodutos/SistemaCadastroProdutos.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemacadastroprodutos;
+
+import br.com.leandro.telas.TelaPrincipal;
+
+/**
+ *
+ * @author leandro
+ */
+public class SistemaCadastroProdutos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Testando o sistema.");
+        //new TelaPrincipal().setVisible(true);
+        TelaPrincipal tela = new TelaPrincipal();
+        tela.setVisible(true);
+    }
+    
+}
